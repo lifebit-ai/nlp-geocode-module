d41358805be2cf19c2c5ac87f16d3b0940b21d0c
Eric Anand <eric@lifebit.ai>
2021-09-15 11:54:54 +0100

    fix: update old function reference

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 from geocoder_module.geocoder import Geocoder
 
 geocoder_module = Geocoder()
-location_info = geocoder_module.get_location_info("sydney")
+location_info = geocoder_module.get_location_info("liverpool", best_matching=False)
 
 double_checked_location_info = geocoder_module.double_check_countries(location_info)
 
