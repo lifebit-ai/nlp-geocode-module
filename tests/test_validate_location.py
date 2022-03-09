5bad7e6bb410c27f87ac6ab8da46ba43003d2fd5
Marcos Cámara Donoso <mcd95alm@gmail.com>
2022-03-09 14:22:03 +0000

    Fix: UK-US location problem (#14)
    * feat: Adds new edges to deal with UK/US problem
    
    * feat: Adds tests for new test cases
    
    * Removes prints

--- tests/test_validate_location.py
+++ tests/test_validate_location.py
@@ -1,3 +1,4 @@
+from unittest import mock
 from unittest.mock import Mock, patch
 import pytest
 
@@ -5,12 +6,15 @@ from geocoder_module.geocoder import Geocoder
 
 geocoder = Geocoder()
 
+# Majority testing
+
 location_output_brussels = {
     "name": "Brussels",
     "bounding_box": [2.224122, 48.902156, 2.4697602, 48.8155755],
     "country": "Belgium",
     "coordinates": [2.3514616, 48.8566969],
 }
+ner_tag_brussels = {"tag": "Brussels", "category": "location"}
 
 location_output_belgium = {
     "name": "Belgium",
@@ -18,6 +22,7 @@ location_output_belgium = {
     "country": "Belgium",
     "coordinates": [-98.4951405, 29.4246002],
 }
+ner_tag_belgium = {"tag": "Belgium", "category": "location"}
 
 location_output_united_states = {
     "name": "United States",
@@ -25,6 +30,7 @@ location_output_united_states = {
     "country": "United States",
     "coordinates": [-98.4951405, 29.4246002],
 }
+ner_tag_united_states = {"tag": "United states", "category": "location"}
 
 location_output_old_paris = {
     "name": "Paris",
@@ -38,16 +44,73 @@ location_output_new_paris = {
     "country": "United States",
     "coordinates": [-95.555513, 33.6617962],
 }
+
+ner_tag_paris = {"tag": "Paris", "category": "location"}
+
 location_output_san_antonio = {
     "name": "San Antonio",
     "bounding_box": [-98.8131865, 29.7309623, -98.2230029, 29.1862572],
     "country": "United States",
     "coordinates": [-98.4951405, 29.4246002],
 }
+
+ner_tag_san_antonio = {"tag": "San Antonio", "category": "location"}
 location_output_texas = {
     "name": "Texas",
     "bounding_box": [-106.6458459, 36.5004529, -93.5078217, 25.83706],
     "country": "United States",
+    "coordinates": [-99.5120986, 31.8160381],
+}
+
+ner_tag_texas = {"tag": "Texas", "category": "location"}
+
+# UK/US Problem
+ner_tag_england = {"tag": "England", "category": "location"}
+ner_tag_london = {"tag": "London", "category": "location"}
+location_output_london_us = {
+    "name": "London",
+    "bounding_box": [-106.6458459, 36.5004529, -93.5078217, 25.83706],
+    "country": "United States",
+    "coordinates": [-99.5120986, 31.8160381],
+}
+location_output_london_uk = {
+    "name": "London",
+    "bounding_box": [-106.6458459, 36.5004529, -93.5078217, 25.83706],
+    "country": "United Kingdom",
+    "coordinates": [-99.5120986, 31.8160381],
+}
+ner_tag_crewe = {"tag": "Crewe", "category": "location"}
+location_output_crewe_uk = {
+    "name": "Crewe",
+    "bounding_box": [-106.6458459, 36.5004529, -93.5078217, 25.83706],
+    "country": "United Kingdom",
+    "coordinates": [-99.5120986, 31.8160381],
+}
+location_output_crewe_us = {
+    "name": "Crewe",
+    "bounding_box": [-106.6458459, 36.5004529, -93.5078217, 25.83706],
+    "country": "United States",
+    "coordinates": [-99.5120986, 31.8160381],
+}
+ner_tag_twekesbury = {"tag": "Twekesbury", "category": "location"}
+location_output_twekesbury_uk = {
+    "name": "twekesbury",
+    "bounding_box": [-106.6458459, 36.5004529, -93.5078217, 25.83706],
+    "country": "United Kingdom",
+    "coordinates": [-99.5120986, 31.8160381],
+}
+ner_tag_eastville = {"tag": "Eastville", "category": "location"}
+location_output_eastville_uk = {
+    "name": "Eastville",
+    "bounding_box": [-106.6458459, 36.5004529, -93.5078217, 25.83706],
+    "country": "United Kingdom",
+    "coordinates": [-99.5120986, 31.8160381],
+}
+ner_tag_united_kingdom = {"tag": "United Kingdom", "category": "location"}
+location_output_united_kingdom = {
+    "name": "United Kingdom",
+    "bounding_box": [-106.6458459, 36.5004529, -93.5078217, 25.83706],
+    "country": "United Kingdom",
     "coordinates": [-99.5120986, 31.8160381],
 }
 
@@ -59,12 +122,13 @@ def test_location_is_returned_valid_if_there_is_only_one_in_article(
     locations = [
         location_output_old_paris,
     ]
+    ner_tags = [ner_tag_paris, ner_tag_belgium]
     # Generate expected output from nlp-api response and add it to mock
 
     mock_get_location_info.return_value = [location_output_old_paris]
 
     # Get response
-    response = geocoder.double_check_countries(locations)
+    response = geocoder.double_check_countries(locations, ner_tags)
 
     expected_output = [location_output_old_paris]
 
@@ -80,12 +144,13 @@ def test_updated_location_is_returned_when_location_is_validated_with_others_in_
         location_output_san_antonio,
         location_output_texas,
     ]
+    ner_tags = [ner_tag_paris, ner_tag_san_antonio, ner_tag_texas]
     # Generate expected output from nlp-api response and add it to mock
 
     mock_get_location_info.return_value = [location_output_new_paris]
 
     # Get response
-    response = geocoder.double_check_countries(locations)
+    response = geocoder.double_check_countries(locations, ner_tags)
 
     expected_output = [
         location_output_new_paris,
@@ -102,10 +167,11 @@ def test_same_location_is_returned_when_location_is_same_than_others_in_article(
         location_output_san_antonio,
         location_output_texas,
     ]
+    ner_tags = [ner_tag_san_antonio, ner_tag_texas]
     # Generate expected output from nlp-api response and add it to mock
 
     # Get response
-    response = geocoder.double_check_countries(locations)
+    response = geocoder.double_check_countries(locations, ner_tags)
 
     expected_output = [
         location_output_san_antonio,
@@ -119,7 +185,12 @@ def test_same_location_is_returned_when_location_is_same_than_others_in_article(
 def test_updated_location_is_returned_when_location_is_same_than_others_in_article_and_only_1_reference_to_country(
     mock_get_location_info,
 ):
-
+    ner_tags = [
+        ner_tag_united_states,
+        ner_tag_paris,
+        ner_tag_san_antonio,
+        ner_tag_texas,
+    ]
     locations = [
         location_output_united_states,
         location_output_san_antonio,
@@ -129,7 +200,7 @@ def test_updated_location_is_returned_when_location_is_same_than_others_in_artic
     # Generate expected output from nlp-api response and add it to mock
     mock_get_location_info.return_value = [location_output_new_paris]
     # Get response
-    response = geocoder.double_check_countries(locations)
+    response = geocoder.double_check_countries(locations, ner_tags)
 
     expected_output = [
         location_output_united_states,
@@ -152,10 +223,17 @@ def test_discarded_location_when_location_is_not_in_countries_locations(
         location_output_texas,
         location_output_old_paris,
     ]
+    ner_tags = [
+        ner_tag_united_states,
+        ner_tag_belgium,
+        ner_tag_paris,
+        ner_tag_san_antonio,
+        ner_tag_texas,
+    ]
     # Generate expected output from nlp-api response and add it to mock
     mock_get_location_info.return_value = [location_output_old_paris]
     # Get response
-    response = geocoder.double_check_countries(locations)
+    response = geocoder.double_check_countries(locations, ner_tags)
 
     expected_output = [
         location_output_united_states,
@@ -181,10 +259,18 @@ def test_retain_all_locations_in_country_when_location_is_in_countries_location(
         location_output_old_paris,
         location_output_brussels,
     ]
+    ner_tags = [
+        ner_tag_united_states,
+        ner_tag_belgium,
+        ner_tag_paris,
+        ner_tag_san_antonio,
+        ner_tag_texas,
+        ner_tag_brussels,
+    ]
     # Generate expected output from nlp-api response and add it to mock
     mock_get_location_info.return_value = [location_output_old_paris]
     # Get response
-    response = geocoder.double_check_countries(locations)
+    response = geocoder.double_check_countries(locations, ner_tags)
 
     expected_output = [
         location_output_united_states,
@@ -199,38 +285,98 @@ def test_retain_all_locations_in_country_when_location_is_in_countries_location(
 
 
 @patch("geocoder_module.geocoder.Geocoder.get_location_info")
-def test__location_is_not_processed_if_no_country_key_in_location(
+def test_location_is_not_processed_if_no_country_key_in_location(
     mock_get_location_info,
 ):
     locations = [{}]
-
+    ner_tags = [ner_tag_belgium, ner_tag_paris]
     # Create dummy return value, though this should not be called
-    mock_get_location_info.return_value = "test"
-
+    mock_get_location_info.json.return_value = "test"
+    mock_get_location_info.json.side_effect = [ner_tag_belgium]
     # Get response
-    response = geocoder.double_check_countries(locations)
+    response = geocoder.double_check_countries(locations, ner_tags)
 
-    assert not mock_get_location_info.called
     assert response == locations
 
 
 @patch("geocoder_module.geocoder.Geocoder.get_location_info")
-def test__location_is_not_processed_if_name_key_in_location(
+def test_location_is_not_processed_if_no_name_key_in_location(
     mock_get_location_info,
 ):
     locations = [
         {"country": "Spain", "bounding_box": [0, 0, 0, 0], "coodinates": [0, 0]}
     ]
+    ner_tags = [ner_tag_belgium, ner_tag_paris]
 
     # Create dummy return value, though this should not be called
-    mock_get_location_info.return_value = "test"
+    mock_get_location_info.json.return_value = "test"
+    mock_get_location_info.json.side_effect = [ner_tag_belgium]
 
     # Get response
-    response = geocoder.double_check_countries(locations)
+    response = geocoder.double_check_countries(locations, ner_tags)
 
     # Expected output
 
     expected_output = [{}]
 
-    assert not mock_get_location_info.called
+    assert response == expected_output
+
+
+@patch("geocoder_module.geocoder.Geocoder.get_location_info")
+def test_location_is_fixed_when_us_uk_problem_happens_and_reference_to_nation_in_tags(
+    mock_get_location_info,
+):
+
+    locations = [
+        location_output_london_us,
+        location_output_twekesbury_uk,
+        location_output_eastville_uk,
+    ]
+    ner_tags = [ner_tag_england]
+    # Generate expected output from nlp-api response and add it to mock
+    mock_get_location_info.return_value = [location_output_london_uk]
+    # Get response
+    response = geocoder.double_check_countries(locations, ner_tags)
+
+    expected_output = [
+        location_output_london_uk,
+        location_output_twekesbury_uk,
+        location_output_eastville_uk,
+    ]
+
+    assert response == expected_output
+
+
+@patch("geocoder_module.geocoder.Geocoder.get_location_info")
+def test_location_is_fixed_when_there_is_tie_and_country_reference_in_ner_tags(
+    mock_get_location_info,
+):
+
+    locations = [
+        location_output_london_us,
+        location_output_crewe_us,
+        location_output_twekesbury_uk,
+        location_output_eastville_uk,
+    ]
+    ner_tags = [ner_tag_united_kingdom]
+    # Generate expected output from nlp-api response and add it to mock
+
+    mock_get_location_info.side_effect = [
+        [location_output_united_kingdom],
+        [location_output_london_uk],
+        [location_output_crewe_uk],
+        [location_output_london_uk],
+        [location_output_crewe_uk],
+    ]
+
+    # Get response
+    response = geocoder.double_check_countries(locations, ner_tags)
+
+    expected_output = [
+        location_output_london_uk,
+        location_output_crewe_uk,
+        location_output_twekesbury_uk,
+        location_output_eastville_uk,
+    ]
+
     assert response == expected_output
