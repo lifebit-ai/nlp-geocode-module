dfc519f2a63b8ad317a73036b4b1520fc0e29c58
Marcos Cámara Donoso <mcd95alm@gmail.com>
2022-05-17 14:26:47 +0100

    adds blacklist.txt to package_data (#22)

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ requirements = open("requirements.txt", "r").readlines()
 
 setup(
     name="geocoder_module",
-    version="0.2",
+    version="0.2.1",
     description="Geocoder module",
     python_requires=">=3.7.6",
     url="https://github.com/lifebit-ai/nlp-geocode-module/",
     author="ML Team | Lifebit",
     packages=["geocoder_module"],
-    package_data={'geocoder_module': ['*.json']},
+    package_data={"geocoder_module": ["*.json", "*.txt"]},
     inclued_package_data=True,
     install_requires=requirements,
 )
