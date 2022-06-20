b1cf466defc26a901090c8c0491e2055cbd9c888
mcamarad <mcd95alm@gmail.com>
2022-06-20 13:32:46 +0100

    Removes asyncio ini, requirements and call from tests

--- tests/test_bounding_box.py
+++ tests/test_bounding_box.py
@@ -6,7 +6,7 @@ geocoder = Geocoder()
 
 
 class TestBoundingBoxIsLarge:
-    async def test_bbox_is_large_x(self):
+    def test_bbox_is_large_x(self):
         bbox = [-178, 0, 178, 0]
         value = geocoder.check_large_bounding_box(bbox)
         assert value == True
@@ -14,7 +14,7 @@ class TestBoundingBoxIsLarge:
         value = geocoder.check_large_bounding_box(bbox)
         assert value == True
 
-    async def test_bbox_is_not_large_x(self):
+    def test_bbox_is_not_large_x(self):
         bbox = [-170, 0, 170, 0]
         value = geocoder.check_large_bounding_box(bbox)
         assert value == False
@@ -22,7 +22,7 @@ class TestBoundingBoxIsLarge:
         value = geocoder.check_large_bounding_box(bbox)
         assert value == False
 
-    async def test_bbox_is_large_y(self):
+    def test_bbox_is_large_y(self):
         bbox = [0, -178, 0, 178]
         value = geocoder.check_large_bounding_box(bbox)
         assert value == True
@@ -30,7 +30,7 @@ class TestBoundingBoxIsLarge:
         value = geocoder.check_large_bounding_box(bbox)
         assert value == True
 
-    async def test_bbox_is_not_large_y(self):
+    def test_bbox_is_not_large_y(self):
         bbox = [0, -170, 0, 170]
         value = geocoder.check_large_bounding_box(bbox)
         assert value == False
